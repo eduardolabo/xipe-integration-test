c91654756dd7370dc9f8ec8b64b659eb491f2e5c
anayarojo <raulmartin_04@hotmail.com>
2023-09-05 09:55:31 -0700

    Implement start tournament function

--- api/src/services/tournament.service.ts
+++ api/src/services/tournament.service.ts
@@ -1,16 +1,36 @@
-import { CreateUserDto } from "../dtos/users.dto";
-import HttpException from "../exceptions/HttpException";
-import trainerModel from "../models/trainer.model";
-import tournamentModel from "../models/tournament.model";
-import pokemonModel from "../models/pokemon.model";
-import {Tournament} from "../models/tournament.model";
-import {CreateTournamentDto} from "../dtos/tournament.dto";
-
+import { CreatePokemonDto } from '../dtos/pokemon.dto';
+import { CreateTournamentDto } from '../dtos/tournament.dto';
+import { CreateTrainerDto } from '../dtos/trainer.dto';
+import { PokemonApiResponse } from '../interfaces/api';
+import pokemonModel, { Pokemon } from '../models/pokemon.model';
+import tournamentModel, { Tournament } from '../models/tournament.model';
+import trainerModel, { Trainer } from '../models/trainer.model';
+import { generateName } from '../utils/names.generator';
+import { random } from '../utils/randomizer';
+import ApiService from './api.service';
+import PokemonService from './pokemon.service';
+import TrainerService from './trainer.service';
 
 class TournamentService {
+    public pokemon = pokemonModel;
     public trainer = trainerModel;
     public tournament = tournamentModel;
-    public pokemon = pokemonModel;
+
+    // public apiService = new ApiService();
+    // public pokemonService = new PokemonService();
+    // public trainerService = new TrainerService();
+    // public tournamentService = new TournamentService();
+
+    private apiService: ApiService;
+    // public pokemonService: PokemonService;
+    // public trainerService: TrainerService;
+
+    constructor(
+        private pokemonService: PokemonService, 
+        private trainerService: TrainerService
+    ) {
+        this.apiService = new ApiService();
+    }
 
     public async saveTournament(tournamentData: CreateTournamentDto): Promise<Tournament> {
         return await (new this.tournament({
@@ -21,23 +41,81 @@ class TournamentService {
             champ: tournamentData.champId,
         })).save() as unknown as Tournament;
     }
+
     public async startTournament(): Promise<Tournament> {
-        //TODO: Create 4 random trainers with their 6 random pokemon consuming pokeapi.co and random data
+        // Create 4 random trainers with their 6 random pokemon consuming pokeapi.co and random data
+        const trainerOneId = await this.getRandomTrainerId();
+        const trainerTwoId = await this.getRandomTrainerId();
+        const trainerThreeId = await this.getRandomTrainerId();
+        const trainerFourId = await this.getRandomTrainerId();
 
+        // Create tournament with 4 random trainers
+        let champId: string | null = null;
+        
+        // Fight 4 trainers until we get a winner
+        const finisherOneId = await this.trainerService.fightTrainer(trainerOneId, trainerTwoId) === 1 
+            ? trainerOneId : trainerTwoId; 
 
-        //TODO: Create tournament with 4 random trainers
+        const finisherTwoId = await this.trainerService.fightTrainer(trainerThreeId, trainerFourId) === 1 
+            ? trainerThreeId : trainerFourId;
+            
+        // Fight 2 winners until we get a champ
+        champId = await this.trainerService.fightTrainer(finisherOneId, finisherTwoId) === 1 
+            ? finisherOneId : finisherTwoId;
 
+        // Save and return Tournament
+        const tournamentData: CreateTournamentDto = {
+            trainerOneId,
+            trainerTwoId,
+            trainerThreeId,
+            trainerFourId,
+            champId,
+        };
 
-        //TODO: Fight 4 trainers until we get a winner
-
-
-        //TODO: Save and return Tournament
-
-
-        //not implemented
-        throw new HttpException(409, "Not implemented");
+        return await this.saveTournament(tournamentData);
     }
 
+    private async getRandomTrainerId(): Promise<string> {
+        const trainerName = generateName();
+
+        // Create trainer if it doesn't exist
+        let trainer = await this.trainer.findOne<Trainer>({name: trainerName});
+        if (!trainer) {
+            const trainerData: CreateTrainerDto = {
+                name: trainerName,
+                pokemonOneId: await this.getRandomPokemonId(),
+                pokemonTwoId: await this.getRandomPokemonId(),
+                pokemonThreeId: await this.getRandomPokemonId(),
+                pokemonFourId: await this.getRandomPokemonId(),
+                pokemonFiveId: await this.getRandomPokemonId(),
+                pokemonSixId: await this.getRandomPokemonId(),
+                isChamp: false,
+            };
+            trainer = await this.trainerService.createTrainer(trainerData);
+        }
+
+        return trainer._id;
+    }
+
+    private async getRandomPokemonId(): Promise<string> {
+        const randomPokemonId = random(1, 150).toString();
+        const response: PokemonApiResponse = await this.apiService.getPokemon(randomPokemonId);
+
+        // Create pokemon if it doesn't exist
+        let pokemon = await this.pokemon.findOne<Pokemon>({name: response.name});
+        if (!pokemon) {
+            const randomLevel = random(1, 100);
+            const pokemonData: CreatePokemonDto = {
+                name: response.name,
+                types: response.types.slice(0, 2).map(type => type.type.name),
+                moves: response.moves.slice(0, 4).map(move => move.move.name),
+                level: randomLevel,
+            }
+            pokemon = await this.pokemonService.createPokemon(pokemonData);
+        }
+
+        return pokemon._id;
+    }
 }
 
 export default TournamentService;
