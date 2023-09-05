2518df42503e92d6b0f802539b36aa9b2186275a
anayarojo <raulmartin_04@hotmail.com>
2023-09-05 09:57:12 -0700

    Implement tournament controller and format controllers code

--- api/src/controllers/pokemon.controller.ts
+++ api/src/controllers/pokemon.controller.ts
@@ -8,28 +8,26 @@ import {
     Route,
     Security,
     Tags,
-} from "tsoa";
-import UserService from "../services/users.service";
-import { Permissions } from "../utils/permissions.utils";
-import PokemonService from "../services/pokemon.service";
-import {CreatePokemonDto} from "../dtos/pokemon.dto";
-import TrainerService from "../services/trainer.service";
-import {CreateTrainerDto} from "../dtos/trainer.dto";
+} from 'tsoa';
+
+import { CreatePokemonDto } from '../dtos/pokemon.dto';
+import { CreateTrainerDto } from '../dtos/trainer.dto';
+import PokemonService from '../services/pokemon.service';
+import TrainerService from '../services/trainer.service';
+import { Permissions } from '../utils/permissions.utils';
 
 @Route("pokemon")
 @Tags("Pokemon")
 export class PokemonController extends Controller {
-    public pokemonService= new PokemonService();
-    public trainerService= new TrainerService();
+    public pokemonService = new PokemonService();
+    public trainerService = new TrainerService();
 
-    constructor() {
-        super();
-    }
     @Get("")
     @Security("jwt", [Permissions.USER_READ])
     public async getPokemons(@Request() request: any) {
         return await this.pokemonService.findAllPokemons();
     }
+
     @Get("/{pokemonId}")
     @Security("jwt", [Permissions.USER_READ])
     public async getPokemon(@Request() request: any, @Path('pokemonId') pokemonId: string) {
